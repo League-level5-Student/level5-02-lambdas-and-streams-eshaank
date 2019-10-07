cf6a890c77944dae65ceee91ee9549629201e8c2
League Student <league@iMac-34.attlocal.net>
2019-10-06 17:26:39 -0700

    asdf
    asdf

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,12 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +23,9 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		addNumbers.addActionListener(e -> addNumbersClick());
+		randNumber.addActionListener(e -> randNumberClick());
+		tellAJoke.addActionListener(e -> tellJokeClick());
 		
 		window.setVisible(true);
 		window.pack();
@@ -28,6 +34,35 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+	void addNumbersClick(){
+	String input1 = JOptionPane.showInputDialog("Enter a number");
+	String input2 = JOptionPane.showInputDialog("Enter another number");
 	
+	int num1 = Integer.parseInt(input1);
+	int num2 = Integer.parseInt(input2);
 	
+	int sum = num1 + num2;
+	JOptionPane.showMessageDialog(null, num1 + " + " + num2 + " = " + sum);
+	}
+	void randNumberClick() {
+		Random rand = new Random();
+		int random = rand.nextInt(101);
+		JOptionPane.showMessageDialog(null, random );
+	}
+	void tellJokeClick(){
+		ArrayList<String> jokeList = new ArrayList<>();
+		jokeList.add("Today at the bank, an old lady asked me to help check her balance. So I pushed her over.");
+		jokeList.add("I bought some shoes from a drug dealer. I don't know what he laced them with, but I've been tripping all day.");
+		jokeList.add("Why is Peter Pan always flying? He neverlands.");
+		jokeList.add("Why do blind people hate skydiving? It scares the hell out of their dogs.");
+		jokeList.add("What do you call a guy with a rubber toe? Roberto.");
+		jokeList.add("Whatdya call a frenchman wearing sandals? Phillipe Phillope.");
+		
+		Random rand = new Random();
+		int random = rand.nextInt(6);
+		
+		JOptionPane.showMessageDialog(null, jokeList.get(random));
+	}
+
+
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -10,14 +10,37 @@ public class LambdaMethods {
 			for(int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
-		}, "repeat");
+		}, "shiva is super mega dumb");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String reverse = "";
+			for(int i = s.length() - 1; 0 <= i; i--) {
+				reverse += s.substring(i, i + 1);
+			}
+			System.out.println(reverse);
+		}, "shiva is super mega dumb");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String upperLower = "";
+			for(int i = 0; i < s.length(); i++) {
+				if(i % 2 == 1) {
+					upperLower += s.substring(i, i + 1).toUpperCase();
+				}
+				else {
+				upperLower +=s.substring(i, i + 1);
+				}
+			}
+			System.out.println(upperLower);
+		}, "shiva is super mega dumb");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String periods = "";
+		for(int i = 0; i < s.length(); i++) {
+			periods += s.substring(i, i+1) + "."; 
+		}
+		System.out.println(periods);
+	}, "shiva is super mega dumb");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
 	
 	}
